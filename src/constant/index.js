117a8f06b9d0b2b4a5c8121caaaf436cdec5f74e
Khai281298 <khailuong61@gmail.com>
2021-07-10 16:41:19 +0700

    #210710: finish

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,2 +1,2 @@
-// export const ENDPOINT = "http://localhost:5000";
-export const ENDPOINT = "https://server-survey-app-clone.herokuapp.com";
+export const ENDPOINT = "http://localhost:5000";
+// export const ENDPOINT = "https://server-survey-app-clone.herokuapp.com";
